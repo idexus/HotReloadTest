e51d52ff20859709ef14285ef2d646a6f7d72162
Pawel Krzywdzinski <pawel@dexus.pl>
2023-02-17 01:25:20 +0100

    update

--- TestPage.cs
+++ TestPage.cs
@@ -12,74 +12,79 @@ public partial class TestPage : ContentPage
         BindingContext = viewModel;
         Resources = AppResources.Default;
 
-        Content = new VStack(out var vStack)
+        Content = new Grid
         {
-            e => e.VerticalOptions(LayoutOptions.Center),
+            e => e.BackgroundColor(Colors.Black),
 
-            new Label("Hot Reload :)")
-                .FontSize(45)
-                .TextColor(Colors.Red)
-                .HorizontalOptions(LayoutOptions.Center)
-                .Configure(label =>
-                {
-                    Task.Run(async () =>
-                    {
-                        await Task.Delay(200);
-                        await label.RotateTo(360, 300);
-                        await vStack.RotateYTo(15, 100);
-                        await vStack.RotateYTo(0, 100);
-                        await vStack.RotateYTo(-15, 100);
-                        await vStack.RotateYTo(0, 100);
-                    });
-                }),
-
-            new Slider(1, 30, 1, out var slider)
-                .Value(e => e.Path("SliderValue"))
-                .Margin(new Thickness(50, 30)),
-
-            new Border
+            new VStack(out var vStack)
             {
-                e => e
-                    .SizeRequest(270, 450)
-                    .StrokeShape(new RoundRectangle().CornerRadius(40))
-                    .BackgroundColor(AppColors.Gray950),
+                e => e.VerticalOptions(LayoutOptions.Center),
 
-                new Grid
+                new Label("Hot Reload :)")
+                    .FontSize(45)
+                    .TextColor(Colors.Red)
+                    .HorizontalOptions(LayoutOptions.Center)
+                    .Configure(label =>
+                    {
+                        Task.Run(async () =>
+                        {
+                            await Task.Delay(200);
+                            await label.RotateTo(360, 300);
+                            await vStack.RotateYTo(15, 100);
+                            await vStack.RotateYTo(0, 100);
+                            await vStack.RotateYTo(-15, 100);
+                            await vStack.RotateYTo(0, 100);
+                        });
+                    }),
+
+                new Slider(1, 30, 1, out var slider)
+                    .Value(e => e.Path("SliderValue"))
+                    .Margin(new Thickness(50, 30)),
+
+                new Border
                 {
-                    e => e.RowDefinitions(e => e.Star().Star(2).Star()),
+                    e => e
+                        .SizeRequest(270, 450)
+                        .StrokeShape(new RoundRectangle().CornerRadius(40))
+                        .BackgroundColor(AppColors.Gray950),
 
-                    new Label()
-                        .Text(e => e.Path("Value").Source(slider).StringFormat("Value : {0:F1}"))
-                        .FontSize(45)
-                        .TextColor(Colors.DarkGray)
-                        .HorizontalOptions(LayoutOptions.Center)
-                        .VerticalOptions(LayoutOptions.Center),
+                    new Grid
+                    {
+                        e => e.RowDefinitions(e => e.Star().Star(2).Star()),
 
-                    new Image("dotnet_bot.png").Row(1),
+                        new Label()
+                            .Text(e => e.Path("Value").Source(slider).StringFormat("Value : {0:F1}"))
+                            .FontSize(45)
+                            .TextColor(Colors.DarkGray)
+                            .HorizontalOptions(LayoutOptions.Center)
+                            .VerticalOptions(LayoutOptions.Center),
 
-                    new Label("Hello, World!").Row(2)
-                        .FontSize(30)
-                        .TextColor(Colors.DarkGray)
-                        .HorizontalOptions(LayoutOptions.Center)
-                        .VerticalOptions(LayoutOptions.Center)
-                }
-            },
+                        new Image("dotnet_bot.png").Row(1),
 
-            new Label()
-                .Text(e => e.Path("Counter"))
-                .FontSize(40)
-                .HorizontalOptions(LayoutOptions.Center)
-                .Margin(10),
+                        new Label("Hello, World!").Row(2)
+                            .FontSize(30)
+                            .TextColor(Colors.DarkGray)
+                            .HorizontalOptions(LayoutOptions.Center)
+                            .VerticalOptions(LayoutOptions.Center)
+                    }
+                },
 
-            new Button("Count")
-                .BackgroundColor(AppColors.Gray950)
-                .TextColor(Colors.White)
-                .FontSize(20)
-                .WidthRequest(270)
-                .OnClicked((Button sender) =>
-                {
-                    viewModel.Count();
-                })
+                new Label()
+                    .Text(e => e.Path("Counter"))
+                    .FontSize(40)
+                    .HorizontalOptions(LayoutOptions.Center)
+                    .Margin(10),
+
+                new Button("Count")
+                    .BackgroundColor(AppColors.Gray950)
+                    .TextColor(Colors.White)
+                    .FontSize(20)
+                    .WidthRequest(270)
+                    .OnClicked((Button sender) =>
+                    {
+                        viewModel.Count();
+                    })
+            }
         };
     }
 }
